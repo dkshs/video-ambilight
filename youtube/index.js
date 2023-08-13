17d48b23007eb9a8d15e35fe23f8c425616df8e5
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2023-08-12 21:51:07 -0300

    adding youtube demo

---
+++ youtube/index.js
@@ -0,0 +1,69 @@
+let video;
+let ambilight;
+let videoId = "_OmLYckdMvI";
+
+function onYouTubeIframeAPIReady() {
+  video = new YT.Player("video", {
+    videoId,
+    events: {
+      onReady: videoReady,
+      onStateChange: videoStateChange,
+    },
+  });
+
+  ambilight = new YT.Player("ambilight", {
+    videoId,
+    events: {
+      onReady: ambilightReady,
+      onStateChange: ambilightStateChange,
+    },
+  });
+}
+
+function videoReady(event) {
+  // event.target.setPlaybackQuality("default");
+}
+
+function videoStateChange(event) {
+  switch (event.data) {
+    case YT.PlayerState.PLAYING:
+      ambilight.seekTo(event.target.getCurrentTime());
+      ambilight.playVideo();
+      break;
+    case YT.PlayerState.PAUSED:
+      ambilight.seekTo(event.target.getCurrentTime());
+      ambilight.pauseVideo();
+      break;
+  }
+}
+
+function optimizeAmbilight(event) {
+  let qualityLevels = event.target.getAvailableQualityLevels();
+  event.target.mute();
+  if (qualityLevels && qualityLevels.length && qualityLevels.length > 0) {
+    qualityLevels.reverse();
+    let lowestLevel =
+      qualityLevels[qualityLevels.findIndex((q) => q !== "auto")];
+    event.target.setPlaybackQuality(lowestLevel);
+  }
+}
+
+function ambilightReady(event) {
+  optimizeAmbilight(event);
+}
+
+function ambilightStateChange(event) {
+  switch (event.data) {
+    case YT.PlayerState.BUFFERING:
+    case YT.PlayerState.PLAYING:
+      optimizeAmbilight(event);
+      break;
+  }
+}
+
+function step() {
+  ambilight.seekTo(video.getCurrentTime());
+  window.requestAnimationFrame(step);
+}
+
+window.requestAnimationFrame(step);
